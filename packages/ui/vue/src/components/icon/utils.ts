ec756d01367fae6e33337025002258c417bf2a46
Yuri Soldatov <bestletter@inbox.ru>
2026-07-09 19:54:27 +0500

    feat: уточнить тип возвращаемого значения функции useIconImport и улучшить обработку ошибок

--- packages/ui/vue/src/components/icon/utils.ts
+++ packages/ui/vue/src/components/icon/utils.ts
@@ -1,15 +1,15 @@
-import { markRaw, defineAsyncComponent } from 'vue'
+import { markRaw, defineAsyncComponent, type Component } from 'vue'
 
 const icons = import.meta.glob('../../../../../icons/src/*.svg')
 
-export function useIconImport(path: string) {
-    // Извлекаем имя файла из пути: '@soldy/icons/check.svg' → 'check.svg'
+export function useIconImport(path: string): Component {
     const name = path.split('/').pop() ?? path
     const loader = icons[`../../../../../icons/src/${name}`]
 
     if (!loader) {
         console.error(`Icon not found: ${path}`)
+        return markRaw({ template: '<svg></svg>' })
     }
 
-    return markRaw(defineAsyncComponent(loader ?? (() => Promise.resolve({ template: '<svg></svg>' }))))
+    return markRaw(defineAsyncComponent(loader as () => Promise<Component>))
 }
