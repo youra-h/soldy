c7ca8039612ed3b0c4dc19d9ecfc962833e7ea9d
Youra <bestletter@inbox.ru>
2026-07-09 21:24:33 +0500

    feat: добавить функцию useIconImport для импорта иконок и обновить индекс композируемых функций

--- packages/ui/vue/src/components/check-box/CheckBox.vue
+++ packages/ui/vue/src/components/check-box/CheckBox.vue
@@ -1,200 +1,209 @@
 <script lang="ts">
-import { TCheckBox, type ICheckBox, type ICheckBoxProps } from '@soldy/core'
-import BaseCheckBox, { syncCheckBox } from './base.component'
-import { useInstance } from '../../composables/useInstance'
-import { useBundle } from '../../composables/useBundle'
-import { useElementBinding } from '../../composables/useElementBinding'
-import { useInstanceBinding } from '../../composables/useInstanceBinding'
-import { useSplitAttrs } from '../../composables/useSplitAttrs'
-import { Icon, useIconImport } from '../icon'
-import { createInputBoolBundle } from '@soldy/plugins'
-import type { TBaseComponentViewProps } from '../component-view'
+import { TCheckBox, type ICheckBox, type ICheckBoxProps } from '@soldy/core';
+import BaseCheckBox, { syncCheckBox } from './base.component';
+import {
+    useInstance,
+    useIconImport,
+    useBundle,
+    useElementBinding,
+    useInstanceBinding,
+    useSplitAttrs,
+} from '../../composables';
+import { Icon } from '../icon';
+import { createInputBoolBundle } from '@soldy/plugins';
+import type { TBaseComponentViewProps } from '../component-view';
 
 export default {
-	name: '_CheckBox',
-	inheritAttrs: false,
-	extends: BaseCheckBox,
-	setup(props: TBaseComponentViewProps<ICheckBoxProps, ICheckBox>, { emit }) {
-		const instance = useInstance(TCheckBox, props)
-		// Инициализация плагинов
-		const plugins = useBundle(createInputBoolBundle, props?.plugins)
-		// Привязка инстанса к плагинам
-		useInstanceBinding(plugins, instance)
-		// Привязка элемента и инстанса к плагинам
-		const rootRef = useElementBinding(plugins)
+    name: '_CheckBox',
+    inheritAttrs: false,
+    extends: BaseCheckBox,
+    setup(props: TBaseComponentViewProps<ICheckBoxProps, ICheckBox>, { emit }) {
+        const instance = useInstance(TCheckBox, props);
+        // Инициализация плагинов
+        const plugins = useBundle(createInputBoolBundle, props?.plugins);
+        // Привязка инстанса к плагинам
+        useInstanceBinding(plugins, instance);
+        // Привязка элемента и инстанса к плагинам
+        const rootRef = useElementBinding(plugins);
 
-		const {
-			rendered,
-			visible,
-			classes,
-			disabled,
-			name,
-			size,
-			indeterminate,
-			plain,
-			value,
-			readonly,
-			required,
-		} = syncCheckBox({
-			props,
-			instance,
-			plugins,
-			emit,
-		})
+        const {
+            rendered,
+            visible,
+            classes,
+            disabled,
+            name,
+            size,
+            indeterminate,
+            plain,
+            value,
+            readonly,
+            required,
+        } = syncCheckBox({
+            props,
+            instance,
+            plugins,
+            emit,
+        });
 
-		// Иконки по умолчанию
-		const defaultIconTag = useIconImport('check')
-		const defaultIndeterminateIconTag = useIconImport('checkIndeterminate')
+        // Иконки по умолчанию
+        const defaultIconTag = useIconImport('check');
+        const defaultIndeterminateIconTag = useIconImport('checkIndeterminate');
 
-		const { containerAttrs, controlAttrs } = useSplitAttrs()
+        const { containerAttrs, controlAttrs } = useSplitAttrs();
 
-		return {
-			containerAttrs,
-			controlAttrs,
-			instance,
-			defaultIconTag,
-			defaultIndeterminateIconTag,
-			plugins,
-			rootRef,
-			rendered,
-			visible,
-			classes,
-			disabled,
-			name,
-			size,
-			indeterminate,
-			plain,
-			value,
-			readonly,
-			required,
-		}
-	},
-}
+        return {
+            containerAttrs,
+            controlAttrs,
+            instance,
+            defaultIconTag,
+            defaultIndeterminateIconTag,
+            plugins,
+            rootRef,
+            rendered,
+            visible,
+            classes,
+            disabled,
+            name,
+            size,
+            indeterminate,
+            plain,
+            value,
+            readonly,
+            required,
+        };
+    },
+};
 </script>
 
 <template>
-	<div ref="rootRef" v-if="rendered" v-show="visible" :class="classes" v-bind="containerAttrs">
-		<input
-			type="checkbox"
-			:id="instance.id.toString()"
-			:checked="value"
-			:name="name"
-			:disabled="disabled"
-			:required="required"
-			:aria-checked="instance.getAriaChecked()"
-			v-bind="controlAttrs"
-		/>
-		<div class="s-check-box__container">
-			<!-- Слот для checked иконки -->
-			<slot
-				v-if="value && !indeterminate"
-				name="icon"
-				:value="value"
-				:indeterminate="indeterminate"
-			>
-				<Icon :tag="defaultIconTag" :size="size" />
-			</slot>
-			<!-- Слот для indeterminate иконки -->
-			<slot
-				v-else-if="indeterminate"
-				name="indeterminate-icon"
-				:value="value"
-				:indeterminate="indeterminate"
-			>
-				<Icon :tag="defaultIndeterminateIconTag" :size="size" />
-			</slot>
-		</div>
-	</div>
+    <div
+        ref="rootRef"
+        v-if="rendered"
+        v-show="visible"
+        :class="classes"
+        v-bind="containerAttrs"
+    >
+        <input
+            type="checkbox"
+            :id="instance.id.toString()"
+            :checked="value"
+            :name="name"
+            :disabled="disabled"
+            :required="required"
+            :aria-checked="instance.getAriaChecked()"
+            v-bind="controlAttrs"
+        />
+        <div class="s-check-box__container">
+            <!-- Слот для checked иконки -->
+            <slot
+                v-if="value && !indeterminate"
+                name="icon"
+                :value="value"
+                :indeterminate="indeterminate"
+            >
+                <Icon :tag="defaultIconTag" :size="size" />
+            </slot>
+            <!-- Слот для indeterminate иконки -->
+            <slot
+                v-else-if="indeterminate"
+                name="indeterminate-icon"
+                :value="value"
+                :indeterminate="indeterminate"
+            >
+                <Icon :tag="defaultIndeterminateIconTag" :size="size" />
+            </slot>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
 @use './mixines' as mixines;
 @use './../../styles/required' as required;
 .s-check-box {
-	$this: &;
+    $this: &;
 
-	@apply w-5 h-5;
-	@apply relative select-none;
+    @apply w-5 h-5;
+    @apply relative select-none;
 
-	input {
-		@apply absolute inset-0 opacity-0;
-		@apply w-full h-full m-0 p-0;
-		@apply cursor-pointer;
-		@apply z-10;
+    input {
+        @apply absolute inset-0 opacity-0;
+        @apply w-full h-full m-0 p-0;
+        @apply cursor-pointer;
+        @apply z-10;
 
-		&:disabled {
-			@apply cursor-default pointer-events-none;
+        &:disabled {
+            @apply cursor-default pointer-events-none;
 
-			+ #{$this}__container {
-				@apply opacity-s-component-disabled;
-			}
-		}
+            + #{$this}__container {
+                @apply opacity-s-component-disabled;
+            }
+        }
 
-		&:focus-visible + #{$this}__container {
-			@apply outline-2 outline-offset-2 outline-s-component;
-		}
-	}
+        &:focus-visible + #{$this}__container {
+            @apply outline-2 outline-offset-2 outline-s-component;
+        }
+    }
 
-	&__container {
-		@apply flex items-center justify-center;
-		@apply w-full h-full rounded-md border;
-		@apply bg-white;
-		@apply transition-colors duration-150;
-	}
+    &__container {
+        @apply flex items-center justify-center;
+        @apply w-full h-full rounded-md border;
+        @apply bg-white;
+        @apply transition-colors duration-150;
+    }
 
-	&--plain {
-		input {
-			&:focus-visible + #{$this}__container {
-				@apply outline-0;
-			}
-		}
+    &--plain {
+        input {
+            &:focus-visible + #{$this}__container {
+                @apply outline-0;
+            }
+        }
 
-		#{$this}__container {
-			@apply border-0;
-			@apply bg-transparent;
-		}
-	}
+        #{$this}__container {
+            @apply border-0;
+            @apply bg-transparent;
+        }
+    }
 
-	&--required {
-		#{$this}__container::after {
-			@include required.required-indicator();
-		}
-	}
+    &--required {
+        #{$this}__container::after {
+            @include required.required-indicator();
+        }
+    }
 
-	&--size-sm {
-		@apply w-4 h-4;
-	}
+    &--size-sm {
+        @apply w-4 h-4;
+    }
 
-	&--size-lg {
-		@apply w-6 h-6;
-	}
+    &--size-lg {
+        @apply w-6 h-6;
+    }
 
-	&--size-xl {
-		@apply w-7 h-7;
-	}
+    &--size-xl {
+        @apply w-7 h-7;
+    }
 
-	&--size-2xl {
-		@apply w-8 h-8;
-	}
+    &--size-2xl {
+        @apply w-8 h-8;
+    }
 
-	&--normal {
-		@include mixines.checkbox-variant($this, $color: 'neutral');
-	}
+    &--normal {
+        @include mixines.checkbox-variant($this, $color: 'neutral');
+    }
 
-	&--accent {
-		@include mixines.checkbox-variant($this, $color: 'accent');
-	}
+    &--accent {
+        @include mixines.checkbox-variant($this, $color: 'accent');
+    }
 
-	&--positive {
-		@include mixines.checkbox-variant($this, $color: 'positive');
-	}
+    &--positive {
+        @include mixines.checkbox-variant($this, $color: 'positive');
+    }
 
-	&--negative {
-		@include mixines.checkbox-variant($this, $color: 'negative');
-	}
+    &--negative {
+        @include mixines.checkbox-variant($this, $color: 'negative');
+    }
 
-	&--caution {
-		@include mixines.checkbox-variant($this, $color: 'caution');
-	}
+    &--caution {
+        @include mixines.checkbox-variant($this, $color: 'caution');
+    }
 }
 </style>
---
+++ packages/ui/vue/src/composables/useIconImport.ts
@@ -0,0 +1,15 @@
+import { defineComponent, h, markRaw, type Component } from 'vue';
+import * as icons from '@soldy/icons';
+
+const iconMap = icons as Record<string, string>;
+
+export function useIconImport(name: string): Component {
+    const svg = iconMap[name];
+
+    if (!svg) {
+        console.error(`Icon not found: ${name}`);
+        return markRaw({ render: () => h('svg') });
+    }
+
+    return markRaw(defineComponent({ template: svg }));
+}
