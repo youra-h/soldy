cc4fa3d7c2bc29ef1de786434bd1cb0896533e62
Yuri Soldatov <bestletter@inbox.ru>
2026-07-13 23:44:35 +0500

    refactor: обновить импорты и заменить контракт на схему для улучшения структуры компонента

--- packages/ui/vue/src/components/component-view/setup.component.ts
+++ packages/ui/vue/src/components/component-view/setup.component.ts
@@ -1,18 +1,26 @@
 import type { SetupContext } from 'vue'
 import { TComponentView, type IComponentViewProps, type IComponentView } from '@soldy/core'
-import { componentViewContract } from '@soldy/schema'
+import { componentViewSchema } from '@soldy/schema'
 import BaseComponentView from './base.component'
 import { useInstance } from '../../composables/useInstance'
-import { vueAdapter } from '../../schema/vueAdapter'
+import { vueAdapter } from '../../adapter/vueAdapter'
 import type { TBaseComponentViewProps } from './types'
 
 export default {
 	name: '_ComponentView',
 	extends: BaseComponentView,
-	setup(props: TBaseComponentViewProps<IComponentViewProps, IComponentView>, { emit }: SetupContext) {
+	setup(
+		props: TBaseComponentViewProps<IComponentViewProps, IComponentView>,
+		{ emit }: SetupContext,
+	) {
 		const instance = useInstance(TComponentView, props)
 
-		const { refs, plugins, rootElement } = vueAdapter(componentViewContract, instance, props, emit)
+		const { refs, plugins, rootElement } = vueAdapter(
+			componentViewSchema,
+			instance,
+			props,
+			emit,
+		)
 
 		return { ctrl: instance, plugins, rootElement, ...refs }
 	},
