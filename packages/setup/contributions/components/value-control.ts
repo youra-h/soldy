b3bc184ee64697ff6aff71231445eba3707bc209
Yuri Soldatov <bestletter@inbox.ru>
2026-07-23 23:25:55 +0500

    refactor: обновить ValueControlContribution, изменить триггеры и события

--- packages/setup/contributions/components/value-control.ts
+++ packages/setup/contributions/components/value-control.ts
@@ -2,8 +2,8 @@ import type { IContribution } from '@soldy/accessor'
 
 export const ValueControlContribution: IContribution = {
 	props: [
-		{ name: 'value', triggers: ['change:value', 'input:value'] },
+		{ name: 'value', triggers: ['change:value'] },
 		{ name: 'name', triggers: ['change:name'] },
 	],
-	events: ['input'],
+	events: ['input', 'input:value'],
 }
