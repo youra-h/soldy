a44b3f0d5ffdc082317568dc8600d4a2bfc19796
Youra <bestletter@inbox.ru>
2026-07-24 21:24:10 +0500

    refactor: добавить props для активного элемента в ActivatableCollectionContribution

--- packages/setup/contributions/components/collection/activable/activatable-collection.ts
+++ packages/setup/contributions/components/collection/activable/activatable-collection.ts
@@ -1,5 +1,8 @@
 import type { IContribution } from '@soldy/accessor'
 
 export const ActivatableCollectionContribution: IContribution = {
+	props: [
+		{ name: 'activeItem', protected: true, triggers: ['item:activated', 'item:deactivated'] },
+	],
 	events: ['item:activated', 'item:deactivated', 'change:activeItem'],
 }
