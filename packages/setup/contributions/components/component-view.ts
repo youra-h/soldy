47f03a2ea197252512ed8b288a403e6f10a46dfe
Youra <bestletter@inbox.ru>
2026-07-27 21:33:40 +0500

    refactor: обновить тип свойства 'tag' в ComponentViewContribution для поддержки массива типов

--- packages/setup/contributions/components/component-view.ts
+++ packages/setup/contributions/components/component-view.ts
@@ -2,7 +2,7 @@ import type { IContribution } from '@soldy/accessor'
 
 export const ComponentViewContribution: IContribution = {
 	props: [
-		{ name: 'tag', type: String, triggers: ['change:tag'] },
+		{ name: 'tag', type: [String, Object], triggers: ['change:tag'] },
 		{ name: 'classes', type: Object, protected: true, triggers: ['change:classes'] },
 	],
 	events: ['ready'],
