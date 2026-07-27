2f8522c7fd4128636018066e648410746b38ed27
Youra <bestletter@inbox.ru>
2026-07-27 21:37:16 +0500

    refactor: добавить свойство 'protected' в props CollapseItemContribution

--- packages/setup/contributions/components/collapse/collapse-item.ts
+++ packages/setup/contributions/components/collapse/collapse-item.ts
@@ -1,7 +1,5 @@
 import type { IContribution } from '@soldy/accessor'
 
 export const CollapseItemContribution: IContribution = {
-	props: [
-		{ name: 'view', type: String, triggers: ['change:view'] },
-	],
+	props: [{ name: 'view', type: String, protected: true, triggers: ['change:view'] }],
 }
