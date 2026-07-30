73fdcf7192169bef14f5256490c36204640c5094
Yuri Soldatov <bestletter@inbox.ru>
2026-07-31 00:47:28 +0500

    refactor(collection): улучшить форматирование и согласованность кода в командах коллекции

--- packages/core/src/components/base/collection/command/insert-command.class.ts
+++ packages/core/src/components/base/collection/command/insert-command.class.ts
@@ -1,22 +1,23 @@
-// command/insert-command.class.ts
+import type { IStorage } from '../storage'
+import type { TEvented } from '../../../../common/event'
+import type { TEngineEvents } from '../types'
+import type { ICommand } from './types'
 
-import type { IStorage } from '../storage';
-import type { TEvented } from '../../../../common/event';
-import type { TEngineEvents } from '../types';
-import type { ICommand } from './types';
-
+/**
+ * Команда вставки элемента в коллекцию. Добавляет элемент в хранилище по указанному индексу и уведомляет о событиях.
+ */
 export class TInsertCommand<T> implements ICommand<T> {
-    constructor(
-        public item: T,
-        public index: number = 0,
-    ) {}
+	constructor(
+		public item: T,
+		public index: number = 0,
+	) {}
 
-    apply(storage: IStorage<T>): void {
-        storage.insert(this.item, this.index);
-    }
+	apply(storage: IStorage<T>): void {
+		storage.insert(this.item, this.index)
+	}
 
-    emitEvents(events: TEvented<TEngineEvents<T>>, storage: IStorage<T>): void {
-        events.emit('item:added', this.item);
-        events.emit('change:count', storage.items.length);
-    }
+	emitEvents(events: TEvented<TEngineEvents<T>>, storage: IStorage<T>): void {
+		events.emit('item:added', this.item)
+		events.emit('change:count', storage.items.length)
+	}
 }
--- packages/core/src/components/base/collection/command/remove-command.class.ts
+++ packages/core/src/components/base/collection/command/remove-command.class.ts
@@ -1,19 +1,20 @@
-// command/remove-command.class.ts
+import type { IStorage } from '../storage'
+import type { TEvented } from '../../../../common/event'
+import type { TEngineEvents } from '../types'
+import type { ICommand } from './types'
 
-import type { IStorage } from '../storage';
-import type { TEvented } from '../../../../common/event';
-import type { TEngineEvents } from '../types';
-import type { ICommand } from './types';
-
+/**
+ * Команда удаления элемента из коллекции. Удаляет элемент из хранилища и уведомляет о событиях.
+ */
 export class TRemoveCommand<T> implements ICommand<T> {
-    constructor(public item: T) {}
+	constructor(public item: T) {}
 
-    apply(storage: IStorage<T>): void {
-        storage.remove(this.item);
-    }
+	apply(storage: IStorage<T>): void {
+		storage.remove(this.item)
+	}
 
-    emitEvents(events: TEvented<TEngineEvents<T>>, storage: IStorage<T>): void {
-        events.emit('item:removed', this.item);
-        events.emit('change:count', storage.items.length);
-    }
+	emitEvents(events: TEvented<TEngineEvents<T>>, storage: IStorage<T>): void {
+		events.emit('item:removed', this.item)
+		events.emit('change:count', storage.items.length)
+	}
 }
