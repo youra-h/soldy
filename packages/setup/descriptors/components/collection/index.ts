70c3045a6b012648745ba164d5e001745a76f39c
Yuri Soldatov <bestletter@inbox.ru>
2026-08-03 22:44:04 +0500

    refactor(components): удалить устаревшие дескрипторы коллекций и элементов коллекций

--- packages/setup/descriptors/components/collection/index.ts
+++
@@ -1,4 +0,0 @@
-export { CollectionDescriptor } from './collection.descriptor'
-export { CollectionItemDescriptor } from './collection-item.descriptor'
-export * from './activable'
-export * from './selectable'
