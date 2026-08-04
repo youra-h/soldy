b826fa473f40b41e6af21a451f4639a496708446
Yuri Soldatov <bestletter@inbox.ru>
2026-08-04 13:45:20 +0500

    refactor(components): изменить тип свойства value на [String, Number, Boolean, Object, Array] в ValueControlContribution

--- packages/setup/contributions/components/value-control.ts
+++ packages/setup/contributions/components/value-control.ts
@@ -2,7 +2,7 @@ import type { IContribution } from '@soldy/accessor'
 
 export const ValueControlContribution: IContribution = {
 	props: [
-		{ name: 'value', type: String, triggers: ['change:value'] },
+		{ name: 'value', type: [String, Number, Boolean, Object, Array], triggers: ['change:value'] },
 		{ name: 'name', type: String, triggers: ['change:name'] },
 	],
 	events: ['input', 'input:value'],
