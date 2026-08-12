2dd72dbf412d7faa173b7c5fec462bf1cb7dfa27
Yuri Soldatov <bestletter@inbox.ru>
2026-08-12 23:32:35 +0500

    refactor(tabs): исправить путь импорта для IItemExtension в типах табов

--- packages/core/src/components/custom/tabs/extensions/item/types.ts
+++ packages/core/src/components/custom/tabs/extensions/item/types.ts
@@ -1,4 +1,4 @@
-import type { IItemExtension } from '../../../base/collection/extension'
+import type { IItemExtension } from '../../../../base/collection'
 
 /**
  * Контракт item-адаптера таба.
@@ -8,4 +8,7 @@ export interface ITabItemExtension<TItem extends object = any>
 	extends IItemExtension<TItem> {
 	/** Может ли таб быть закрыт. */
 	readonly closable: boolean
+
+	/** Закрыть таб. Делегирует в родительское расширение. */
+	close(): void
 }
