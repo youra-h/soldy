229a49e6b5f90fb9a49d618a76edfdcf83d7a05b
Yuri Soldatov <bestletter@inbox.ru>
2026-09-20 00:15:23 +0500

    remove test

--- packages/accessor/accessor.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { IAccessorEvent } from './contract'
-import type { TProperty } from './property.class'
-
-/** Базовый контракт accessor'а */
-export interface IAccessor {
-	getProps(includeProtected?: boolean): TProperty[]
-	getEvents(): IAccessorEvent[]
-}
